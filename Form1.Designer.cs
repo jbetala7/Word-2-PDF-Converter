0baa7da9d1290e215b771c6251d7179baff6ae86
jbetala7 <jayesh.betala7@gmail.com>
2019-09-30 23:57:20 +0530

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Word_to_PDF_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.BackColor = System.Drawing.Color.Firebrick;
+            this.btnConvert.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnConvert.Location = new System.Drawing.Point(595, 471);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(213, 31);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(125, 471);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(912, 598);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Word 2 PDF Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace Word_to_PDF_Converter
+{
+    public partial class Form1 : Form
+    {
+        string docFileName = string.Empty, defaultPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnConvert.Enabled = false;
+            defaultPath = Path.GetDirectoryName(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+            defaultPath = Path.Combine(defaultPath, "Desktop");
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.Filter = "PDF Files|*.pdf";
+            saveDialog.Title = "PDF file to be saved";
+            saveDialog.InitialDirectory = defaultPath;
+            saveDialog.FileName = Path.GetFileNameWithoutExtension(docFileName) + ".pdf";
+            if (saveDialog.ShowDialog() == DialogResult.OK && !string.IsNullOrEmpty(saveDialog.FileName))
+            {
+                string outPdfFile = saveDialog.FileName;
+                try
+
+                {
+                    object readOnly = true;
+                    _Application wordApp = new Microsoft.Office.Interop.Word.Application();
+                    wordApp.Visible = false;
+                    Document oDoc = wordApp.Documents.Open(docFileName, System.Reflection.Missing.Value, readOnly);
+                    oDoc.Activate();
+                    object format = WdSaveFormat.wdFormatPDF;
+                    oDoc.SaveAs2(outPdfFile, format);
+                    wordApp.Quit();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnConvert.Enabled = false;
+                docFileName = string.Empty;
+                MessageBox.Show("File successfully converted.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.InitialDirectory = defaultPath;
+            openDialog.Title = "Select file to be converted";
+            openDialog.Filter = "Word 2007 Documents (*.docx)|*.docx|Word 97-2003 Documents (*.doc)|*.doc";
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                docFileName = openDialog.FileName;
+                btnConvert.Enabled = true;
+            }
+        }
+    }
+}
